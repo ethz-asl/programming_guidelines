e29c7b7510682ca17488f17741d0cf0d9dbabf66
HannesSommer <hannes.sommer@mavt.ethz.ch>
2014-05-06 16:01:28 +0200

    improved usability of commentOutUnusedParameters by using Getopt::Long and introducing some options.

--- codeCleaningTools/commentOutUnusedParameters.pl
+++ codeCleaningTools/commentOutUnusedParameters.pl
@@ -1,13 +1,31 @@
 #!/usr/bin/env perl
 
+use Getopt::Long;
+my $force   = 0;
+my $prefix = "";
+my $help =  0;
+my $verbose = 0;
+my $debug = 0;
+$result = GetOptions (
+                    "prefix|p=s"   => \$prefix,
+                    "replacement|r=s" => \$prefixReplacement,
+                    "force|f"  => \$force,
+                    "verbose|v"  => \$verbose,
+                    "debug|d"  => \$debug,
+                    "help|h|usage|u" => \$help
+                    )
+	or $help = 1;
+
+if($help){
+	print "USAGE: $ARGV[0] [(-h|--help)] | [-f|--force] [-v|--verbose] [-p|--prefix <PREFIX>] [-r|--replacement <PREFIX_REPLACEMENT>] [<INPUT_FILE>]\n";
+	exit(-1);
+}
+
 #Just feed in build output on stdin. 
 #Tested mostly with Jenkins raw build output (http://129.132.38.183:8080/job/????/consoleText).
 #runs best directly in the git work tree.
 
 #use the two first positional arguments to replace path prefixes:
-
-$prefix = (scalar @ARGV) > 0 ? $ARGV[0] : "";
-$prefixReplacement = (scalar @ARGV) > 1 ? $ARGV[1] : "";
 
 %ids=(); 
 
@@ -23,7 +41,7 @@ if(!$currentRev) {
 
 $revisionOk = 0;
 
-while (<STDIN>){
+while (<>){
 	$line = $_;
 	if($line =~/^ *Checking out Revision /){
 		my ($rev, $rest) = ($line =~/^ *Checking out Revision *([[:alnum:]]+)(.*)/i);
@@ -31,7 +49,7 @@ while (<STDIN>){
 			print "Jenkins used commit '$rev'.\n";
 			if($currentRev && $currentRev ne $rev){
 				print STDERR "Don't use compiler warnings from a different commit ($rev (Jenkins) != $currentRev ($pwd)! The line numbers may not match.\nTip: checkout commit $rev first!\n";
-				exit -1;
+				if(!$force) { exit -1; }
 			}
 		}
 	}
@@ -40,27 +58,36 @@ while (<STDIN>){
 		if($file) {
 			$id = "$file:$lineNr:$name";
 			if($ids{$id}) { 
-				print "skipping duplicate : $id\n";
+				!$verbose or print "skipping duplicate : $id\n";
 				next; 
 			}
 			else { $ids{$id} = 1;}
 			
 			if(index($file, $prefix) == 0){
 				$file=~ s/^\Q$prefix\E/$prefixReplacement/;
+				!$verbose or print "processing unused parameter $id in $file\n";
+				
 				if( -e $file ){
-					$matcher = "$name\\( *\\([,)=]\\|\$\\)\\)";
-					$replacement = "/* $name */";
-					$ret = system("sed -n '${lineNr},/{/p' '$file' | sed -n '\\#$matcher#q1'");
-					if(system("sed -n '${lineNr},/{/p' '$file' | sed -n '\\#/\\* *$name *\\*/#q1'") eq 256) {
+					$testAlreadyCmd = "sed -n '${lineNr},/{/p' '$file' | sed -n '\\#/\\* *$name *\\*/#q1; /{/q0'";
+					$ret = system($testAlreadyCmd);
+					!$debug or print "testing whether already applied with : $testAlreadyCmd -> $ret\n";
+					if($ret eq 256) {
 						print "replacing '$name' : already applied.\n";
-					}elsif($ret eq 256){
-						print "replacing '$name'.\n";
-						system("sed -i '${lineNr},/{/{s#$matcher#$replacement\\1#; t quit; b}; b; :quit; n; b quit' '$file'");
-					}else{
-						print STDERR "$file:$lineNr :";
-						print STDERR "'WARNING: failed to replace '$name' in:";
-						system("sed -n '${lineNr}p' '$file' >&2");
-						print STDERR "compiler warning: $_";
+					}else {
+						$matcher = "$name\\( *\\([,)=]\\|\$\\)\\)";
+						$cmd = "sed -n '${lineNr},/{/p' '$file' | sed -n '\\#$matcher#q1'";
+						$ret = system($cmd);
+						!$debug or print "testing whether we can replace with : $cmd -> $ret\n";
+						if($ret eq 256){
+							$replacement = "/* $name */";
+							print "replacing '$name' with '$replacement'.\n";
+							system("sed -i '${lineNr},/{/{s#$matcher#$replacement\\1#; t quit; b}; b; :quit; n; b quit' '$file'");
+						}else{
+							print STDERR "$file:$lineNr :";
+							print STDERR "'WARNING: failed to replace '$name' in:\n";
+							system("sed -n '${lineNr},/{/p' '$file' >&2");
+							print STDERR "compiler warning: $_";
+						}
 					}
 				}
 				else {
