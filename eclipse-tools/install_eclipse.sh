ca2384fedc3f027b30fe265bfbaa3571bf1023c6
Pascal Gohl <pascal.gohl@gmail.com>
2014-08-25 19:25:54 +0200

    give eclipse more memory

--- eclipse-tools/install_eclipse.sh
+++ eclipse-tools/install_eclipse.sh
@@ -14,6 +14,11 @@ sudo chown $USER -R /opt/eclipse/
 sudo ln -sf /opt/eclipse/eclipse /usr/sbin/eclipse
 rm eclipse-cpp-luna-R-linux-gtk-x86_64.tar.gz
 
+# give eclipse more memory
+sudo sed -i "s/-XX:MaxPermSize=256m/-XX:MaxPermSize=1024m/g" /opt/eclipse/eclipse.ini
+sudo sed -i "s/-Xms40m/-Xms512m/g" /opt/eclipse/eclipse.ini
+sudo sed -i "s/-Xmx512m/-Xmx1024m/g" /opt/eclipse/eclipse.ini
+
 # setup unity link
 cat > eclipse.desktop << "EOF"
 [Desktop Entry]
